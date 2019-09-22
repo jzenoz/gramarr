d399b00063eb557f5f4e60f86ae81664ec26a6e8
Tom Smith <tom@t-smith.co.uk>
2019-09-22 10:19:53 +0100

    Removing unsed code

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,49 +1,6 @@
 package util
 
-import (
-	"fmt"
-	"strings"
-
-	"github.com/tommy647/gramarr/internal/users"
-	tb "gopkg.in/tucnak/telebot.v2"
-)
-
-func Send(bot *tb.Bot, to tb.Recipient, msg string) {
-	bot.Send(to, msg, tb.ModeMarkdown)
-}
-
-func SendError(bot *tb.Bot, to tb.Recipient, msg string) {
-	bot.Send(to, msg, tb.ModeMarkdown)
-}
-
-func SendAdmin(bot *tb.Bot, to []users.User, msg string) {
-	SendMany(bot, to, fmt.Sprintf(`*[Admin]* %s`, msg))
-}
-
-func SendKeyboardList(bot *tb.Bot, to tb.Recipient, msg string, list []string) error {
-	var buttons []tb.ReplyButton
-	for _, item := range list {
-		buttons = append(buttons, tb.ReplyButton{Text: item})
-	}
-
-	var replyKeys [][]tb.ReplyButton
-	for _, b := range buttons {
-		replyKeys = append(replyKeys, []tb.ReplyButton{b})
-	}
-
-	_, err := bot.Send(to, msg, &tb.ReplyMarkup{
-		ReplyKeyboard:   replyKeys,
-		OneTimeKeyboard: true,
-	})
-
-	return err
-}
-
-func SendMany(bot *tb.Bot, to []users.User, msg string) {
-	for _, user := range to {
-		bot.Send(user, msg, tb.ModeMarkdown)
-	}
-}
+import "strings"
 
 func EscapeMarkdown(s string) string {
 	s = strings.Replace(s, "[", "\\[", -1)
